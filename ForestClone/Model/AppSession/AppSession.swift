c3cfb3110ec48efe4800c35587682fc2e0aaaedb
Christian Ray Leovido <christian.leovido@gmail.com>
2020-06-26 15:42:11 +0100

    Create AppSession.swift

---
+++ ForestClone/Model/AppSession/AppSession.swift
@@ -0,0 +1,20 @@
+//
+//  AppSession.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 26/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+
+enum AppSession {
+
+    static var user: User = User(id: "9f136642-110d-4302-bac2-e58443730584",
+                          name: "Christian",
+                          coins: [],
+                          achievements: [],
+                          currentSession: nil,
+                          completedSessions: [])
+
+}
