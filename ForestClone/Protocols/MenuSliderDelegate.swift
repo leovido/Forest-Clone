1da83ab8c2d6f9f8589759b7daff673f9b8647f3
Christian Ray Leovido <christian.leovido@gmail.com>
2020-06-28 14:43:01 +0100

    Create MenuSliderDelegate.swift

---
+++ ForestClone/Protocols/MenuSliderDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  MenuSliderDelegate.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 27/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+
+protocol MenuSliderDelegate: class {
+    func performSegue(with name: String)
+}
