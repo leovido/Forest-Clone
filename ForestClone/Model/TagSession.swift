59cc0e21beec75f472f9489596b6144b2ae7064b
Christian Ray Leovido <christian.leovido@gmail.com>
2020-06-28 14:42:57 +0100

    Create TagSession.swift

---
+++ ForestClone/Model/TagSession.swift
@@ -0,0 +1,14 @@
+//
+//  TagSession.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 28/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+
+struct TagSession: Codable {
+    var name: String
+    var color: String
+}
