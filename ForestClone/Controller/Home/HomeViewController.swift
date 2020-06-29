f376f536f9041dafd920504bfed0fe6ec51036d1
Christian Ray Leovido <christian.leovido@gmail.com>
2020-06-29 18:52:12 +0100

    refactoring

--- ForestClone/Controller/Home/HomeViewController.swift
+++ ForestClone/Controller/Home/HomeViewController.swift
@@ -19,8 +19,22 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var phraseLabel: UILabel!
     @IBOutlet weak var currentTreeImage: UIImageView!
 
-    @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var plantButton: UIButton!
+
+    @IBOutlet weak var giveUpButton: UIButton!
+    @IBAction func giveUpButton(_ sender: Any) {
+
+        customAlertPresenter.presenter.presentFailureAlert()
+        timer?.invalidate()
+        notificationScheduler.cancelExistingNotification(id: session.id)
+        firebaseFocusSessionStorage.update(id: session.id, data: ["status": "cancelled"]) { updatedSession in
+            print(updatedSession)
+        }
+
+        timerLabel.text = timeFormatted(time: session.time)
+        session.status = .cancelled
+
+    }
 
     // - MARK: IBActions
     @IBAction func menuButton(_ sender: Any) {
@@ -28,6 +42,7 @@ class HomeViewController: UIViewController {
     }
 
     let notificationScheduler: NotificationScheduler = NotificationScheduler()
+    let customAlertPresenter: CustomAlertPresenter = CustomAlertPresenter()
 
     weak var timer: Timer!
 
@@ -35,7 +50,19 @@ class HomeViewController: UIViewController {
     var timerValue: Int! = 600
 
     var currentUser = AppSession.user
-    var session = FocusSession(status: .idle)
+    var session = FocusSession(status: .idle) {
+        didSet {
+            if session.status == .started {
+                plantButton.isHidden = true
+                giveUpButton.isHidden = false
+                slider.alpha = 0
+            } else {
+                plantButton.isHidden = false
+                giveUpButton.isHidden = true
+                slider?.alpha = 1
+            }
+        }
+    }
     let treeSpecimenManager = TreeSpecimenManager()
 
     // - MARK: Firebase Database Storage
@@ -48,32 +75,21 @@ class HomeViewController: UIViewController {
 
     @IBAction func plantButton(_ sender: Any) {
 
-        if session.status != .started {
+        phraseLabel.text = generateRandomPhrase()
+        timer = processTimer()
 
-            phraseLabel.text = generateRandomPhrase()
-            timer = processTimer()
+        _ = notificationScheduler
+            .createSuccessNotification(with: session.time, and: session.id)
+            .map({ success in
 
-            notificationScheduler.createSuccessNotification(with: 5,
-                                                            and: session.id)
+                if success {
+                    self.session.date = Date()
+                    self.session.status = .started
 
-            session.date = Date()
-            session.status = .started
-            plantButton.setTitle("Give Up", for: .normal)
+                    self.createFirebaseEntry()
+                }
 
-            createFirebaseEntry()
-
-        } else {
-
-            presentAlert(type: .failure)
-            timer?.invalidate()
-            notificationScheduler.cancelExistingNotification(id: session.id)
-
-            slider?.alpha = 1
-            timerLabel.text = timeFormatted(time: session.time)
-            session.status = .cancelled
-            plantButton.setTitle("Plant", for: .normal)
-
-        }
+            })
 
     }
 
@@ -103,32 +119,19 @@ class HomeViewController: UIViewController {
         setupNotificationCenter()
         configureTapForTreeSelection()
 
-    }
-
-    func updateTimerState() {
-
-         // 1. Calculate the time elapsed from the existing session if it's already started and not completed.
-
-        if self.session.status == .started {
-            let timeElapsed = Date().timeIntervalSince(self.session.date)
-            self.timerValue = Int(timeElapsed)
-
-            self.timer = self.processTimer()
-        } else {
-            self.timerLabel.text = self.timeFormatted(time: self.timerValue)
-        }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Menu", style: .plain, target: self, action: #selector(menuSidebarConfiguration))
 
     }
 
     @objc func treeManager() {
-
+        customAlertPresenter.presenter.presentItems()
     }
 
     func configureTapForTreeSelection() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(treeManager))
 
-        currentTreeImage.isUserInteractionEnabled = true
-        currentTreeImage.addGestureRecognizer(tapGestureRecognizer)
+        slider.isUserInteractionEnabled = true
+        slider.addGestureRecognizer(tapGestureRecognizer)
     }
 
     func setupNotificationCenter() {
@@ -149,92 +152,6 @@ class HomeViewController: UIViewController {
         }
 
         return randomPhrase
-    }
-
-    private func menuSidebarConfiguration() {
-        let menu = storyboard!.instantiateViewController(
-            withIdentifier: "SideMenuNavigationController"
-            ) as! SideMenuNavigationController
-
-        menu.presentationStyle = .menuDissolveIn
-        menu.menuWidth = 200
-        menu.blurEffectStyle = .dark
-
-        present(menu, animated: true, completion: nil)
-    }
-
-    func timeFormatted(time: Seconds) -> String {
-
-        let minutes: Int = time / 60
-        let seconds: Int = time % 60
-
-        return String(format: "%02i:%02i", minutes, seconds)
-    }
-
-    func processTimer() -> Timer {
-
-        // 1. Hide the slider
-        slider?.alpha = 0
-
-        // 2. Return a new Timer with custom logic
-        return Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-
-            // 3. When it reaches 0, we show the slider again and invalidate the Timer.
-            if self.timerValue == 0 {
-
-                // 4. TODO: Present an alert with a reward
-
-                // 5. Show the slider
-                self.slider?.alpha = 1
-                self.timerLabel.text = self.timeFormatted(time: self.timerValue)
-
-                self.timer.invalidate()
-
-            } else {
-
-                // 3.1. Update the timerLabel.text
-                self.timerValue -= 1
-                self.timerLabel.text = self.timeFormatted(time: self.timerValue)
-
-                // 3.2. Update phrase every 15 seconds
-
-                if self.timerValue % 15 == 0 {
-                    self.phraseLabel.text = phrases[.default]!.randomElement()!
-                }
-            }
-
-        }
-
-    }
-
-    func displayTime(value: Double) -> String {
-        Int(value).description + ":00"
-    }
-
-    func presentAlert(type: ForestAlert) {
-
-        var alert: UIAlertController!
-
-        if type == .failure {
-            alert = UIAlertController(title: "It'll be better next time!",
-                                          message: "",
-                                          preferredStyle: .alert)
-
-            alert.addAction(UIAlertAction(title: "Ok",
-                                          style: .default,
-                                          handler: nil))
-        } else {
-            alert = UIAlertController(title: "You've Got",
-                                          message: "",
-                                          preferredStyle: .alert)
-
-            alert.addAction(UIAlertAction(title: "Ok",
-                                          style: .default,
-                                          handler: nil))
-        }
-
-
-        self.present(alert, animated: true, completion: nil)
     }
 
 }
