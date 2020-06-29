f7bd6e4adde276859fa490cd926d78a63a7f02d1
Christian Ray Leovido <christian.leovido@gmail.com>
2020-06-29 18:51:41 +0100

    Create ForestSegue.swift

---
+++ ForestClone/Model/ForestSegue.swift
@@ -0,0 +1,14 @@
+//
+//  ForestSegue.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 28/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+
+enum ForestSegue: String {
+    case forest = "forestSegue"
+    case achievements = "achievementsSegue"
+}
