d2a255937bb5cce8636ef3a63b44073bf08986fe
Christian Ray Leovido <christian.leovido@gmail.com>
2020-06-29 18:51:19 +0100

    Create ForestAlertCustom.swift

---
+++ ForestClone/Controller/Home/Extensions/Alerts/ForestAlertCustom.swift
@@ -0,0 +1,37 @@
+//
+//  ForestAlertCustom.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 29/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+import SCLAlertView
+
+protocol AlertPresentable {
+    func presentSuccessAlert()
+    func presentFailureAlert()
+    func presentItems()
+}
+
+extension SCLAlertView: AlertPresentable {}
+
+extension AlertPresentable where Self: SCLAlertView {
+    func presentSuccessAlert() {
+        showSuccess("You've Got", subTitle: "This tree")
+    }
+
+    func presentFailureAlert() {
+        showError("It'll be better next time!", subTitle: "")
+    }
+
+    func presentItems() {
+        showSuccess("You've Got", subTitle: "This tree", circleIconImage: UIImage(systemName: "person"))
+
+    }
+}
+
+final class CustomAlertPresenter {
+    let presenter: AlertPresentable = SCLAlertView()
+}
