a7c500726667d40a8ec5eca1404cabb608afef88
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 21:36:04 +0100

    Create LoginViewController.swift

---
+++ ForestClone/Controller/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 01/07/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextfield: UITextField!
+
+    @IBOutlet weak var passwordTextfield: UITextField!
+
+    @IBAction func loginButton(_ sender: Any) {
+
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
