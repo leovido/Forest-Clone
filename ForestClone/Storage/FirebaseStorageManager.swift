b5f0474cc06f196bf9468f8cd0923109eae1463b
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 18:53:28 +0100

    Create FirebaseStorageManager.swift

---
+++ ForestClone/Storage/FirebaseStorageManager.swift
@@ -0,0 +1,29 @@
+//
+//  FirebaseStorageManager.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 30/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+
+protocol Storage {
+
+    associatedtype T
+    associatedtype U
+
+    var sessionStorage: T { get }
+    var userStorage: U { get }
+}
+
+final class FirebaseStorageManager<T: NetworkingRequestable, U: NetworkingRequestable>: Storage {
+
+    let sessionStorage: T
+    let userStorage: U
+
+    init(sessionStorage: T, userStorage: U) {
+        self.sessionStorage = sessionStorage
+        self.userStorage = userStorage
+    }
+}
