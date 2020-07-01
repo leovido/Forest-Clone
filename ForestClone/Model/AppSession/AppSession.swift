f1ec09cde662ad928e51db4364843adab9e55fc8
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 18:57:30 +0100

    add default user (should be updated with Firebase add state listener)

--- ForestClone/Model/AppSession/AppSession.swift
+++ ForestClone/Model/AppSession/AppSession.swift
@@ -10,11 +10,12 @@ import Foundation
 
 enum AppSession {
 
-    static var user: User = User(id: "9f136642-110d-4302-bac2-e58443730584",
-                          name: "Christian",
-                          coins: [],
-                          achievements: [],
-                          currentSession: nil,
-                          completedSessions: [])
+    static var user: FirebaseUserRepresentable = User(userId: "9f136642-110d-4302-bac2-e58443730584",
+                                                      displayName: "Christian",
+                                                      email: "test@test.com",
+                                                      coins: 1000,
+                                                      achievements: [],
+                                                      currentSession: nil,
+                                                      completedSessions: [])
 
 }
