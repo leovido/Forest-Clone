12c93577a2d1e0d2aa5ceea858aab4ea546a686f
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 21:36:18 +0100

    set white text color

--- ForestClone/View/MenuCellTableViewCell.swift
+++ ForestClone/View/MenuCellTableViewCell.swift
@@ -17,13 +17,8 @@ class MenuCellTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        menuItemLabel.textColor = .white
     }
 
 }
