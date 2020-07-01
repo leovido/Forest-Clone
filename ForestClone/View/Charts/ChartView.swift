7ce11c9e691406390c02e2a6ea073c79b35299d9
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 18:52:33 +0100

    Create ChartView.swift

---
+++ ForestClone/View/Charts/ChartView.swift
@@ -0,0 +1,17 @@
+//
+//  ChartView.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 26/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+class ChartView: BarChartView {
+
+    func view() {
+        self.barData
+    }
+}
