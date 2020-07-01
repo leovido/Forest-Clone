57a39679a3d41acacf5e01480cf16e37ae534b0b
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 18:52:38 +0100

    Create SessionStorage.swift

---
+++ ForestClone/Storage/SessionStorage.swift
@@ -0,0 +1,94 @@
+//
+//  SessionStorage.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 30/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+final class SessionStorage: NetworkingService, FirebaseInterface {
+
+    typealias Model = FocusSession
+    typealias Service = DatabaseReference
+
+    let referenceName: String = "focusSession"
+    var modelReference: DatabaseReference
+
+    init() {
+        self.modelReference = Database.database().reference().child(referenceName)
+    }
+
+    func create(data: [String : Any], completion: @escaping (Result<Bool, Error>) -> Void) {
+
+        guard let id = data["focusSessionId"] as? String else {
+            fatalError("JSON needs a focusSessionId")
+        }
+
+        modelReference.child(id).updateChildValues(data) { error, ref in
+
+            if let error = error {
+                completion(Result.failure(error))
+            } else {
+                completion(Result.success(true))
+            }
+        }
+    }
+
+    func read(id: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
+
+        guard let value = modelReference.value(forKey: id) as? [String: Any] else {
+            return
+        }
+
+        completion(Result.success(value))
+
+    }
+
+    func readAll(completion: @escaping (Result<[[String : Any]], Error>) -> Void) {
+        
+        modelReference.observe(.value, with: { snap in
+            guard let value = snap.value as? [String: Any] else {
+                return
+            }
+
+            let values = value.compactMap({ $0.value as? [String: Any] })
+
+            completion(Result.success(values))
+
+        }) { error in
+            print(error)
+        }
+    }
+
+
+    func update(id: String, data: [String : Any], completion: @escaping (Result<[String: Any], Error>) -> Void) {
+
+        modelReference.child(id).updateChildValues(data) { error, ref in
+            if let error = error {
+                completion(Result.failure(error))
+            } else {
+
+                self.modelReference
+                    .child(id)
+                    .observeSingleEvent(of: .value, with: { snapshot in
+
+                    guard let json = snapshot.value as? [String: Any] else {
+                        return
+                    }
+
+                    completion(Result.success(json))
+
+                })
+
+            }
+        }
+    }
+
+    func delete(id: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        modelReference.child(id).removeValue()
+    }
+
+}
