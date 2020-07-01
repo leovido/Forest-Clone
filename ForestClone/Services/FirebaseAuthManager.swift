a041714a38d3a06f58f64ce16ce5e712644bfe15
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 18:53:31 +0100

    Create FirebaseAuthManager.swift

---
+++ ForestClone/Services/FirebaseAuthManager.swift
@@ -0,0 +1,89 @@
+//
+//  FirebaseAuthManager.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 30/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+protocol AuthenticationRepresentable {
+
+    var semaphore: DispatchSemaphore { get }
+
+    func signup(email: String, password: String) -> Result<AuthDataResult, Error>
+    func signin(email: String, password: String, completion: @escaping () -> Void)
+    func signout() throws
+
+}
+
+final class FirebaseAuthManager<T: NetworkingService> {
+
+    let storage: T
+    let semaphore = DispatchSemaphore(value: 0)
+
+    init(storage: T) {
+        self.storage = storage
+    }
+
+    func setUserListener() {
+        _ = Auth.auth().addStateDidChangeListener { (auth, user) in
+
+            guard let user = user else {
+                return
+            }
+
+            self.storage.read(id: user.uid) { userModel in
+//                AppSession.user = userModel
+            }
+        }
+    }
+
+    func signout() throws {
+        try Auth.auth().signOut()
+    }
+
+    func signup(email: String, password: String) -> Result<AuthDataResult, Error> {
+
+        var result: Result<AuthDataResult, Error>!
+
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+
+            if let error = error {
+                result = Result.failure(error)
+                self.semaphore.signal()
+            }
+
+            guard let authResult = authResult else {
+                return
+            }
+
+            result = Result.success(authResult)
+            self.semaphore.signal()
+
+        }
+
+        _ = semaphore.wait(wallTimeout: .distantFuture)
+
+        return result
+    }
+
+    func signin(email: String, password: String, completion: @escaping () -> Void) {
+
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+
+            guard let authResult = authResult else {
+                return
+            }
+
+            completion()
+        }
+
+    }
+}
