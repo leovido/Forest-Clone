da0fe9f453c54b064a5ce679d505058625c9dbc7
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 18:52:32 +0100

    Create PieChartView.swift

---
+++ ForestClone/View/Charts/PieChartView.swift
@@ -0,0 +1,14 @@
+//
+//  PieChartView.swift
+//  ForestClone
+//
+//  Created by Christian Leovido on 26/06/2020.
+//  Copyright © 2020 Accent IT Services. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+class CustomPieChartView: PieChartView {
+
+}
