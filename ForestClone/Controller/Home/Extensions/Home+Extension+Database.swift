4c00328ff9d3378efe7744c42d6bce1c289e9b42
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-01 18:59:18 +0100

    update with viewModel

--- ForestClone/Controller/Home/Extensions/Home+Extension+Database.swift
+++ ForestClone/Controller/Home/Extensions/Home+Extension+Database.swift
@@ -11,93 +11,84 @@ import CodableFirebase
 
 extension HomeViewController {
 
-    func createFirebaseEntry() {
-        self.firebaseFocusSessionStorage.create(entry: self.session) { success in
-            if success {
+    func createFirebaseEntry() throws {
+        viewModel.service.create(data: try session.encodeToJSON()) { result in
+
+            switch result {
+                case .success:
                 print("Successfully create a new entry in Firebase")
-            } else {
-                print("Something happened and it wasn't stored in Firebase")
+
+                case .failure(let error):
+                    print("Something happened and it wasn't stored in Firebase: \(error.localizedDescription)")
             }
         }
     }
 
     func fetchLatestSession(completion: @escaping (Bool) -> Void) {
 
-        firebaseFocusSessionStorage.modelReference
-            .observeSingleEvent(of: .value, with: { snapshot in
+        viewModel.service.readAll { result in
 
-            guard let value = snapshot.value as? [String: Any] else {
+            switch result {
+                case .success(let values):
+
+                do {
+
+                    let models = try FirebaseDecoder()
+                        .decode([FocusSession].self, from: values)
+                        .filter({ $0.status == .started })
+                        .sorted(by: { $0.date < $1.date })
+
+                    guard let latestActiveSession = models.last else {
+                        return
+                    }
+
+                    // Check if the latest session has finished by comparing the Date
+                    let expectedDateFinished = latestActiveSession.date.addingTimeInterval(TimeInterval(latestActiveSession.time))
+
+                    if expectedDateFinished > Date() {
+                        self.session = latestActiveSession
+                        self.session.focusSessionId = latestActiveSession.focusSessionId
+                        completion(true)
+                    } else {
+                        self.viewModel.service
+                            .update(id: self.session.focusSessionId,
+                                    data: ["status": "completed"]) { updatedSession in
+
+                            // update user sessions here...
+
+                            completion(true)
+
+                        }
+                    }
+
+                } catch let error {
+                    print(error)
+
+                    completion(false)
+
+                }
+
+                case .failure:
                 completion(false)
+            }
+        }
+    }
+
+    func updateUserStorage() {
+        do {
+
+            guard let user = self.currentUser as? User else {
                 return
             }
 
-            // transform the dictionary to get an Array of values.
-            let values = value.compactMap({ $0.value })
-
-            do {
-                let models = try FirebaseDecoder()
-                    .decode([FocusSession].self, from: values)
-                    .sorted(by: { $0.date < $1.date })
-
-                guard let latestActiveSession = models.last else {
-                    return
-                }
-
-                // Check if the latest session has finished by comparing the Date
-                let expectedDateFinished = latestActiveSession.date.addingTimeInterval(TimeInterval(latestActiveSession.time))
-
-                if expectedDateFinished > Date() {
-                    self.session = latestActiveSession
-                    self.session.id = latestActiveSession.id
-                    completion(true)
-                } else {
-                    self.firebaseFocusSessionStorage.update(id: self.session.id, data: ["status": "completed"]) { updatedSession in
-
-                        // update user sessions here...
-
-                    }
-                }
-
-            } catch let error {
-                print(error)
-
-                completion(false)
-
-            }
-
-        })
-
-    }
-
-    @objc func updateSession(notification: Notification) {
-
-        guard let sessionInfo = notification.userInfo else {
-            return
-        }
-
-        guard let focusSessionId = sessionInfo["focusSessionId"] as? String else {
-            return
-        }
-
-        firebaseFocusSessionStorage.update(id: focusSessionId, data: ["status": FocusSession.FocusSesionStatus.completed.rawValue]) { focusSession in
-
-            self.currentUser.completedSessions.append(focusSession)
-
-            self.updateUserStorage()
-
-        }
-    }
-
-    private func updateUserStorage() {
-        do {
-            let data = try JSONEncoder().encode(self.currentUser)
+            let data = try JSONEncoder().encode(user)
 
             guard let jsonUser = try JSONSerialization
                 .jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
                     return
             }
 
-            self.firebaseUserStorage.update(id: self.currentUser.id,
+            self.firebaseUserStorage.update(id: self.currentUser.userId,
                                             data: jsonUser) { _ in
 
                                                 print("user updated")
